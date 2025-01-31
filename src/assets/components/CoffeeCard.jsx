39dbf02c29f889fdefc4b9c1375cedd888131e1d
jaky-sheikh <jaky@eu4m.eu>
2025-01-31 12:26:58 +0600

    delete and update

--- src/assets/components/CoffeeCard.jsx
+++ src/assets/components/CoffeeCard.jsx
@@ -1,8 +1,42 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const CoffeeCard = ({ coffee }) => {
 
-    const { name, quantity, category, details, photo } = coffee;
+    const { _id, name, quantity, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-sm">
@@ -15,7 +49,13 @@ const CoffeeCard = ({ coffee }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{details}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Details</button>
+                    <div className="join join-vertical">
+                        <button className="btn join-item">View</button>
+                        <Link to={`updateCoffee/${_id}`}>
+                            <button className="btn join-item">Edit</button>
+                        </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn join-item bg-red-500">X</button>
+                    </div>
                 </div>
             </div>
         </div>
